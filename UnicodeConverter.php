626125945c35c56a73e0692c6926259d3a047e8f
Alex Monk <krenair@gmail.com>
2015-05-24 14:32:40 +0100

    Add extension.json, empty old PHP entry point, remove i18n shim
    Bug: T88066
    Change-Id: I9096a39be8219ebcda38a8d4fe8839b4675c401d

--- UnicodeConverter.php
+++ UnicodeConverter.php
@@ -1,26 +1,14 @@
 <?php
-
-# This is a simple example of a special page module
-# Given a string in UTF-8, it converts it to HTML entities suitable for
-# an ISO 8859-1 web page.
-
-# Not a valid entry point, skip unless MEDIAWIKI is defined
-if (!defined('MEDIAWIKI')) {
-	echo "Unicode Converter extension";
-	exit( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'UnicodeConverter' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['UnicodeConverter'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['UnicodeConverterAlias'] = __DIR__ . '/UnicodeConverter.alias.php';
+	/* wfWarn(
+		'Deprecated PHP entry point used for UnicodeConverter extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
+} else {
+	die( 'This version of the UnicodeConverter extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['specialpage'][] = array(
-	'path' => __FILE__,
-	'name' => 'UnicodeConverter',
-	'author' => 'Tim Starling',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:UnicodeConverter',
-	'descriptionmsg' => 'unicodeconverter-desc',
-);
-
-$dir = __DIR__ . '/';
-$wgMessagesDirs['UnicodeConverter'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['UnicodeConverter'] = $dir . 'UnicodeConverter.i18n.php';
-$wgExtensionMessagesFiles['UnicodeConverterAlias'] = $dir . 'UnicodeConverter.alias.php';
-$wgAutoloadClasses['SpecialUnicodeConverter'] = $dir . 'UnicodeConverter_body.php';
-$wgSpecialPages['UnicodeConverter'] = 'SpecialUnicodeConverter';
