4529afd5f03106fd250024c45051a648d9b77485
Umherirrender <umherirrender_de.wp@web.de>
2017-02-04 22:17:29 +0100

    Fix typo in special page alias
    Change-Id: Ic171d235fd90bee2217cfdd6b0504ce0069b2c38

--- UnicodeConverter.alias.php
+++ UnicodeConverter.alias.php
@@ -7,23 +7,23 @@
 $specialPageAliases = array();
 
 $specialPageAliases['en'] = array(
-	'UnicodeConvertor' => array( 'UnicodeConvertor' ),
+	'UnicodeConverter' => array( 'UnicodeConverter', 'UnicodeConvertor' ),
 );
 
 /** Arabic (العربية)
  * @author Meno25
  */
 $specialPageAliases['ar'] = array(
-	'UnicodeConvertor' => array( 'محول_يونيكود' ),
+	'UnicodeConverter' => array( 'محول_يونيكود' ),
 );
 
 /** Egyptian Spoken Arabic (مصرى)
  * @author Meno25
  */
 $specialPageAliases['arz'] = array(
-	'UnicodeConvertor' => array( 'محول_يونيكود' ),
+	'UnicodeConverter' => array( 'محول_يونيكود' ),
 );
 
 $specialPageAliases['nl'] = array(
-	'UnicodeConvertor' => array( 'UnicodeConverteren' ),
+	'UnicodeConverter' => array( 'UnicodeConverteren' ),
 );
